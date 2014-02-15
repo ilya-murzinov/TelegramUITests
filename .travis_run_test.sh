3ca40016e2a77aafbdaea2fb86be8878aa790089
murz42 <murz42@gmail.com>
2014-02-15 21:43:14 +0400

    edited script for test running

--- .travis_run_test.sh
+++ .travis_run_test.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
 adb push bin/TelegramUITests.jar /data/local/tmp
-adb shell uiautomator runtest TelegramUITests.jar -e class org.telegram.tests.Tests
+adb shell uiautomator runtest TelegramUITests.jar -e class org.telegram.tests.Tests | grep 'FAILURES!!!' &> /dev/null
+if [$? != 0]; then
+    exit 0
+fi
