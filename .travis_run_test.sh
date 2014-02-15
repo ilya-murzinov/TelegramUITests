2c4464116bd32fa1fb8924630278306856ea3b2f
murz42 <murz42@gmail.com>
2014-02-16 01:26:18 +0400

    edited .travis.yml

--- .travis_run_test.sh
+++ .travis_run_test.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
 
-#adb push bin/TelegramUITests.jar /data/local/tmp
-a=$(adb shell uiautomator runtest TelegramUITests.jar -e class org.telegram.tests.Tests | grep 'FAILURES!!!')
-if [ "${a}"=='' ]; then
-    exit 0;
-fi
-exit 1;
+adb push bin/TelegramUITests.jar /data/local/tmp
+adb shell uiautomator runtest TelegramUITests.jar -e class org.telegram.tests.Tests
