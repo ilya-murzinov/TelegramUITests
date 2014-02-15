23008eaf314bd01332fcd9c6b387046df822a71f
murz42 <murz42@gmail.com>
2014-02-16 01:33:11 +0400

    edited .travis.yml

--- .travis_run_test.sh
+++ .travis_run_test.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
 adb push bin/TelegramUITests.jar /data/local/tmp
-adb shell uiautomator runtest TelegramUITests.jar -e class org.telegram.tests.Tests
+if adb shell uiautomator runtest TelegramUITests.jar -e class org.telegram.tests.Tests | grep 'FAILURES!!!'==''; then
+    exit 0;
+fi
+echo 'One or more tests failed!'
+exit 1;
