f9b2dc056e0e3fa5a1d2b2fcabbd54746abf94c2
murz42 <murz42@gmail.com>
2014-02-16 13:34:40 +0400

    changed api to 19

--- .travis_start_emulator.sh
+++ .travis_start_emulator.sh
@@ -2,5 +2,5 @@
 
 # Scripts starts Android emulator with name 'Default'
 
-echo no | android create avd --force -n Default -t android-17 --abi armeabi-v7a
+echo no | android create avd --force -n Default -t android-19 --abi armeabi-v7a
 emulator -avd Default -no-skin -no-audio -no-window &
