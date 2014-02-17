fe269160c6995b53e92684676197e5c4eeecbed6
Ilya Murzinov <ilya-murzinov@users.noreply.github.com>
2014-02-16 23:05:09 -0800

    Update .travis_run_test.sh

--- .travis_run_test.sh
+++ .travis_run_test.sh
@@ -2,11 +2,10 @@
 
 adb install app/org.telegram.android-2.apk
 adb push bin/TelegramUITests.jar /data/local/tmp
-#a=$(
 adb shell uiautomator runtest TelegramUITests.jar -e class org.telegram.tests.Tests
-#if [ -z ${a} ]; then
-#    echo 'All tests passed!'
-#    exit 0;
-#fi
-#echo 'One or more tests failed!'
-#exit 1;
+if [[ "$?" =~ 'FAILURES!!!' ]; then
+    echo 'One or more tests failed!'
+    exit 1;
+fi
+echo 'All tests passed!'
+exit 0;
